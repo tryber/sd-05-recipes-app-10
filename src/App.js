7eedd70d621b0144900d13b326eac8095d24f115
Natalia Macedo <nataliasilvamacedo@gmail.com>
2020-09-09 09:36:35 -0300

    Component Login

--- src/App.js
+++ src/App.js
@@ -1,16 +1,19 @@
 import React from 'react';
 import './App.css';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import Login from './pages/Login';
 
 function App() {
   return (
-    <BrowserRouter>
-      <Switch>
-        <Route exact path="/" component={login} />
-        <Route exact path="/comidas" component={comidas} />
-        <Route exact path="/bebidas" component={bebidas} />
-      </Switch>
-    </BrowserRouter>
+    <div>
+      <BrowserRouter>
+        <Switch>
+          <Route exact path="/" component={Login} />
+          {/* <Route exact path="/comidas" component={comidas} />
+          <Route exact path="/bebidas" component={bebidas} /> */}
+        </Switch>
+      </BrowserRouter>
+    </div>
   );
 }
 
--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,13 +1,12 @@
-class  extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {  }
-    }
-    render() { 
-        return (
-            <div>teste</div>
-        );
-    }
+import React from 'react';
+
+export default function Login() {
+  return (
+    <div>
+      <h1>Login</h1>
+      <input type="email" placeholder="Email" data-testid="email-input" />
+      <input type="text" placeholder="Senha" data-testid="password-input" minLength="6" />
+      <button data-testid="login-submit-btn" type="button" disabled>Entrar</button>
+    </div>
+  );
 }
- 
-export default ;
