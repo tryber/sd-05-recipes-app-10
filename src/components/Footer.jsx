f767423ad84234265d3075f19c24ef5c4195d79f
Paulo-Dandrea <prog.dandrea@gmail.com>
2020-09-09 12:22:37 -0300

    categorias para bebidas e comidas, sem fetch api ainda

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,22 +1,29 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { Link } from 'react-router-dom';
 import drinkIcon from '../images/drinkIcon.svg';
 import exploreIcon from '../images/exploreIcon.svg';
 import mealIcon from '../images/mealIcon.svg';
 import './Footer.css';
+import { ReceitasContext } from '../Context/ReceitasContext';
 
 const Footer = () => {
+  const { setQualAPI } = useContext(ReceitasContext);
+
+  const whichPageFnc = (page) => {
+    setQualAPI(page)
+  }
+
   return (
     <div className="footer" data-testid="footer">
-        <Link to="bebidas" data-testid="drinks-bottom-btn">
-          <img src={drinkIcon}></img>
-        </Link>
-        <Link to="explorar" data-testid="explore-bottom-btn">
-          <img src={exploreIcon}></img>
-        </Link>
-        <Link to="comidas" data-testid="food-bottom-btn">
-          <img src={mealIcon}></img>
-        </Link>
+      <Link to="bebidas">
+        <img data-testid="drinks-bottom-btn" onClick={() => whichPageFnc('drink')} src={drinkIcon}></img>
+      </Link>
+      <Link to="explorar">
+        <img data-testid="explore-bottom-btn" src={exploreIcon}></img>
+      </Link>
+      <Link to="comidas">
+        <img data-testid="food-bottom-btn" onClick={() => whichPageFnc('meal')} src={mealIcon}></img>
+      </Link>
     </div>
   );
 };
--- src/pages/Bebidas.jsx
+++ src/pages/Bebidas.jsx
@@ -1,8 +1,10 @@
 import React, { Fragment } from 'react';
 import Footer from '../components/Footer';
+import MealCategories from '../components/MealCategories';
 
 const Bebidas = () => {
   return (<Fragment>Bebidas
+    <MealCategories />
     <Footer />
   </Fragment>);
 }
