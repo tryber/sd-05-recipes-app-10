b2688ed9f9d2bf285f992ee9329d701227d0f0bc
Natalia Macedo <nataliasilvamacedo@gmail.com>
2020-09-09 19:35:46 -0300

    Add infor no context de login

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,15 +1,16 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useContext } from 'react';
+import { ReceitasContext } from '../Context/ReceitasContext';
+import { Link } from 'react-router-dom';
 
 export default function Login() {
-  const [email, setEmail] = useState('');
-  const [senha, setSenha] = useState('');
+  const { email, setEmail, senha, setSenha } = useContext(ReceitasContext);
   const [isDisabled, isSetDisabled] = useState(true);
 
 
   // Fonte regex https://www.devmedia.com.br/iniciando-expressoes-regulares/6557
   function validaInput(xEmail, xSenha) {
     const regexEmail = /^\w+([.-]?\w+)@\w+([.-]?\w+)(.\w{2,3})+$/;
-    const regexSenha = /^\w{4,10}$ ^[a-zA-Z]\w{3,9}$ ^[a-zA-Z]\w*\d+\w*$/;
+    const regexSenha = /^[^W_]{7,100}$/;
 
     if (regexEmail.test(xEmail) && regexSenha.test(xSenha)) {
       isSetDisabled(false);
@@ -17,6 +18,12 @@ export default function Login() {
       isSetDisabled(true);
     }
   }
+
+  const saveStorage = () => {
+    localStorage.setItem('mealsToken', 1);
+    localStorage.setItem('cocktailsToken', 1);
+    localStorage.setItem('email', JSON.stringify({ email }));
+  };
 
   useEffect(() => {
     validaInput(email, senha);
@@ -29,7 +36,6 @@ export default function Login() {
         type="email"
         placeholder="Email"
         data-testid="email-input"
-        value={email}
         onChange={(e) => setEmail(e.target.value)}
       />
       <input
@@ -37,12 +43,13 @@ export default function Login() {
         placeholder="Senha"
         data-testid="password-input"
         minLength="6"
-        value={senha}
         onChange={(e) => setSenha(e.target.value)}
       />
-      <button type="submit" data-testid="login-submit-btn" disabled={isDisabled}>
-        Entrar
-      </button>
+      <Link to="/comidas">
+        <button type="submit" data-testid="login-submit-btn" disabled={isDisabled} onClick={() => saveStorage()}>
+          Entrar
+        </button>
+      </Link>
     </div>
   );
 }
