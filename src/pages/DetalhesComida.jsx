46b4c18d766480ca5cc425f4645ea92adbc33230
Marylange <marylangess@gmail.com>
2020-09-11 17:33:04 -0300

    corrigindo CC2

--- src/pages/DetalhesComida.jsx
+++ src/pages/DetalhesComida.jsx
@@ -4,80 +4,80 @@ import shareIcon from '../images/shareIcon.svg';
 import whiteHeartIcon from '../images/whiteHeartIcon.svg';
 import '../style/DetalhesComida.css';
 
+function handleIngredients(mealDB) {
+  return (
+    <div>
+      <h3>Ingredients</h3>
+      <ul>
+        {Object.entries(mealDB.recipeDetails).map((element, index) => {
+          if (element[0].includes('strIngredient') && element[1] !== '') {
+            return (
+              <li data-testid={`${index}-ingredient-name-and-measure`}>{element[1]}</li>
+            );
+          }
+        })}
+      </ul>
+    </div>
+  );
+}
+
+// function handleStrMeasure() {
+//   return (
+//     <div>
+//       <ul>
+//         {Object.entries(mealDB.recipeDetails).map((element, index) => {
+//           if (element[0].includes('strMeasure') && element[1] !== ' ') {
+//             return (
+//               <li data-testid={`${index}-ingredient-name-and-measure`}>{element[1]}</li>
+//             );
+//           }
+//         })}
+//       </ul>
+//     </div>
+//   );
+// }
+
+function handleStrInstructions(mealDB) {
+  return (
+    <div>
+      <h3>Instructions</h3>
+      <p>{mealDB.recipeDetails.strInstructions}</p>
+    </div>
+  );
+}
+
+function handleStrYoutube(mealDB) {
+  return (
+    <div>
+      <h3>Vídeo</h3>
+      <video width="320" height="240" controls>
+        <source data-testid="video" src={mealDB.recipeDetails.strYoutube} type="video/mp4" />
+      </video>
+    </div>
+  );
+}
+
+function handleRecommendationsDrinks(drinkDB) {
+  return (
+    <div>
+      <h3>Recomendadas</h3>
+      {drinkDB.recommendDrinks.slice(0, 6).map((drink) => (
+        <div className="drinks-card-details">
+          <img alt="drinks" className="img-drinks" src={drink.strDrinkThumb} />
+          <div className="container">
+            <p>{drink.strAlcoholic}</p>
+            <h4>{drink.strDrink}</h4>
+          </div>
+        </div>
+      ))}
+    </div>
+  );
+}
+
 const DetalhesComida = () => {
   const { mealDB, drinkDB } = useContext(ReceitasContext);
   console.log('mealDB', mealDB);
   console.log('drinks', drinkDB.recommendDrinks);
-
-  function handleIngredients() {
-    return (
-      <div>
-        <h3>Ingredients</h3>
-        <ul>
-          {Object.entries(mealDB.recipeDetails).map((element, index) => {
-            if (element[0].includes('strIngredient') && element[1] !== '') {
-              return (
-                <li data-testid={`${index}-ingredient-name-and-measure`}>{element[1]}</li>
-              );
-            }
-          })}
-        </ul>
-      </div>
-    );
-  }
-
-  // function handleStrMeasure() {
-  //   return (
-  //     <div>
-  //       <ul>
-  //         {Object.entries(mealDB.recipeDetails).map((element, index) => {
-  //           if (element[0].includes('strMeasure') && element[1] !== ' ') {
-  //             return (
-  //               <li data-testid={`${index}-ingredient-name-and-measure`}>{element[1]}</li>
-  //             );
-  //           }
-  //         })}
-  //       </ul>
-  //     </div>
-  //   );
-  // }
-
-  function handleStrInstructions() {
-    return (
-      <div>
-        <h3>Instructions</h3>
-        <p>{mealDB.recipeDetails.strInstructions}</p>
-      </div>
-    );
-  };
-
-  function handleStrYoutube() {
-    return (
-      <div>
-        <h3>Vídeo</h3>
-        <video width="320" height="240" controls>
-          <source data-testid="video" src={mealDB.recipeDetails.strYoutube} type="video/mp4" />
-        </video>
-      </div>
-    );
-  }
-
-  function handleRecommendationsDrinks() {
-    return (
-      <div>
-        <h3>Recomendadas</h3>
-        {drinkDB.recommendDrinks.slice(0, 6).map((drink) => (
-          <div className="drinks-card-details">
-            <img alt="drinks" className="img-drinks" src={drink.strDrinkThumb} />
-            <div className="container">
-              <p>{drink.strAlcoholic}</p>
-              <h4>{drink.strDrink}</h4>
-            </div>
-          </div>
-        ))}
-      </div>
-    );
-  }
 
   return (
     <div>
@@ -98,12 +98,12 @@ const DetalhesComida = () => {
         <img alt="favorite button" data-testid="favorite-btn" src={whiteHeartIcon} />
       </div>
       <div>
-        {handleIngredients()}
+        {handleIngredients(mealDB)}
         {/* {handleStrMeasure()} */}
       </div>
-      {handleStrInstructions()}
-      {handleStrYoutube()}
-      {handleRecommendationsDrinks()}
+      {handleStrInstructions(mealDB)}
+      {handleStrYoutube(mealDB)}
+      {handleRecommendationsDrinks(drinkDB)}
     </div>
   );
 };
