cd7e1733dc361fb8684e00ba88fde60d5c76e445
Paulo-Dandrea <prog.dandrea@gmail.com>
2020-09-11 11:00:22 -0300

    list of MEALS almost completely passing the test. IT needs to: render list of drinks and the ALL category

--- src/components/RecipeCard.jsx
+++ src/components/RecipeCard.jsx
@@ -2,12 +2,12 @@ import React from 'react';
 import './RecipeCard.css';
 
 const RecipeCard = (props) => {
-  const {dataTestId, recipe: {strMeal, strMealThumb}} = props;
+  const { testName, testIt, TestIdImage, recipe: {strMeal, strMealThumb}} = props;
 
   return ( 
-  <div className="card" data-testid={dataTestId} key={dataTestId}>
-    <img src={strMealThumb} style={{'width': '200px'}}alt=""/>
-    <h3>{strMeal}</h3>
+  <div className="card" data-testid={testIt} key={testIt}>
+    <img src={strMealThumb} data-testid={TestIdImage} style={{'width': '200px'}}alt=""/>
+    <h3 data-testid={testName}> {strMeal}</h3>
   </div> );
 }
  
--- src/components/RecipesListByCategory.jsx
+++ src/components/RecipesListByCategory.jsx
@@ -5,18 +5,30 @@ import { fetchMealsFilterdByCategory } from '../services/ApiRequest';
 import RecipeCard from './RecipeCard';
 
 const RecipesListByCategory = () => {
-  const { category, recipesFiltered, setRecipesFiltered } = useContext(ReceitasContext);
+  const { category, recipesFiltered, setRecipesFiltered } = useContext(
+    ReceitasContext,
+  );
 
   useEffect(() => {
-    fetchMealsFilterdByCategory(category).then(e => setRecipesFiltered(e))
-  }, [category])
+    fetchMealsFilterdByCategory(category).then((e) => setRecipesFiltered(e));
+  }, [category]);
 
   return (
-  <Fragment>
-    {recipesFiltered.slice(0,12).map((recipe, index) => <RecipeCard dataTestId={`${index}-card-img`}recipe={recipe}/>)}
-  </Fragment>  );
-}
- 
+    <Fragment>
+      {recipesFiltered.slice(0, 12).map((recipe, index) => {
+        return (
+          <RecipeCard
+            testIt={`${index}-recipe-card`}
+            testName={`${index}-card-name`}
+            TestIdImage={`${index}-card-img`}
+            recipe={recipe}
+          />
+        );
+      })}
+    </Fragment>
+  );
+};
+
 export default RecipesListByCategory;
 
 // fetchMealsFilterdByCategory data-testid=`${index}-card-img`
